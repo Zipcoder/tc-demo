d9efda31a380baac5db49b808327d2c915f9548f
Leon <leon@Leons-MacBook-Pro.local>
2018-07-05 01:39:08 -0400

    update

--- src/main/java/rocks/zipcodewilmington/week4/p07052018Wednesday/MyCollection.java
+++ src/main/java/rocks/zipcodewilmington/week4/p07052018Wednesday/MyCollection.java
@@ -1,19 +1,24 @@
 package rocks.zipcodewilmington.week4.p07052018Wednesday;
 
 import java.util.AbstractCollection;
+import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.List;
 
 /**
  * @author leon on 7/5/18.
  */
 public class MyCollection<E> extends AbstractCollection<E>  {
+    private final Iterable<E> iterable;
+    public MyCollection(Iterable<E> iterable) {  this.iterable = iterable; }
+
     @Override
-    public Iterator<E> iterator() {
-        return null;
-    }
+    public Iterator<E> iterator() {  return iterable.iterator(); }
 
     @Override
     public int size() {
-        return 0;
+        List<E> list = new ArrayList<>();
+        iterator().forEachRemaining(list::add);
+        return list.size();
     }
 }
