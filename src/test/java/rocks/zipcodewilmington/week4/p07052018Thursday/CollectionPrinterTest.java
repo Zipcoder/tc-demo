c78c96857d2318ceb9ca8f47ad156892450e78d2
Leon <leon@Leons-MacBook-Pro.local>
2018-07-05 00:42:40 -0400

    it's late and i'm sleepy; i hope this makes sense tomorrow morning... :+1

---
+++ src/test/java/rocks/zipcodewilmington/week4/p07052018Thursday/CollectionPrinterTest.java
@@ -0,0 +1,7 @@
+package rocks.zipcodewilmington.week4.p07052018Thursday;
+
+/**
+ * @author leon on 7/4/18.
+ */
+public class CollectionPrinterTest {
+}
---
+++ src/test/java/rocks/zipcodewilmington/week4/p07052018Thursday/TestSet.java
@@ -0,0 +1,7 @@
+package rocks.zipcodewilmington.week4.p07052018Thursday;
+
+/**
+ * @author leon on 7/5/18.
+ */
+public class TestSet {
+}
